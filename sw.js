150957ea56a2856cc25555d155da3ec9e3c57465
Hevin40 <hevinmgolakiya4@gmail.com>
2026-05-01 16:31:08 +0530

    Fix iOS Profil screen tap-dead bug: remove overflow:hidden that broke click events

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
-const VERSION = 'v68';
+const VERSION = 'v69';
 const STATIC_CACHE = 'buscar-static-' + VERSION;
 const RUNTIME_CACHE = 'buscar-runtime-' + VERSION;
 
 const STATIC_ASSETS = [
   './',
   './index.html',
-  './styles.css?v=68',
-  './app.js?v=68',
+  './styles.css?v=69',
+  './app.js?v=69',
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
